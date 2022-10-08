90272dd6b095893a4616009c9c8a491cae94ae9a
Randall <108246257+randalltr@users.noreply.github.com>
2022-10-08 22:46:53 +0700

    started workshop 3 comment form modal

---
+++ src/features/comments/CommentForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
+
+const CommentForm = ({campsiteId}) => {
+    
+    const [modalOpen, setModalOpen] = useState(false);
+
+    return (
+        <>
+            <Button outline onClick={() => setModalOpen(true)}>
+                <i className='fa fa-pencil fa-lg' /> Add Comment
+            </Button>
+            <Modal isOpen={modalOpen}>
+                <ModalHeader toggle={() => setModalOpen(false)}>Add Comment</ModalHeader>
+                <ModalBody>campsite: {campsiteId}</ModalBody>
+            </Modal>
+        </>
+    );
+}
+
+export default CommentForm;
