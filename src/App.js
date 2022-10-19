c0edf40f328bb4a62c3f062c40e5fd4bb556c1d1
Randall <108246257+randalltr@users.noreply.github.com>
2022-10-19 10:33:25 +0700

    updated App.js with async campsites

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -9,6 +12,13 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
